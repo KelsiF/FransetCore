158311f6fa357e43085775f8fdb2b8907003e292
Kelsi <superigromen2018@gmail.com>
2023-11-24 16:10:25 +0300

    Add files via upload

--- src/main/java/org/kelsi/fransetcore.java
+++ src/main/java/org/kelsi/fransetcore.java
@@ -1,21 +1,21 @@
 package org.kelsi;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kelsi.commands.*;
-import org.kelsi.other.christmas;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 
 public final class fransetcore extends JavaPlugin implements Listener {
+
+    public static Map<UUID, Integer> rep = new HashMap<>();
 
     @Override
     public void onEnable(){
@@ -23,11 +23,18 @@ public final class fransetcore extends JavaPlugin implements Listener {
         //for(Player player : Bukkit.getOnlinePlayers()) {
         //public List<String> disabled_users = new ArrayList<String>();
         getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
         Objects.requireNonNull(getCommand("broadcast")).setExecutor(new broadcastCommand());
-        getCommand("me").setExecutor(new meCommand());
-        getCommand("do").setExecutor(new doCommand());
-        getCommand("try").setExecutor(new tryCommand());
-        getCommand("roll").setExecutor(new rollCommand());
+        Objects.requireNonNull(getCommand("me")).setExecutor(new meCommand());
+        Objects.requireNonNull(getCommand("do")).setExecutor(new doCommand());
+        Objects.requireNonNull(getCommand("try")).setExecutor(new tryCommand());
+        Objects.requireNonNull(getCommand("roll")).setExecutor(new rollCommand());
+        Objects.requireNonNull(getCommand("setevent")).setExecutor(new seteventCommand(this));
+        Objects.requireNonNull(getCommand("removeevent")).setExecutor(new removeeventCommand(this));
+        Objects.requireNonNull(getCommand("event")).setExecutor(new eventCommand(this));
+        Objects.requireNonNull(getCommand("rep")).setExecutor(new repCommand());
+        Objects.requireNonNull(getCommand("world")).setExecutor(new worldCommand());
 
        // }
     }
@@ -37,25 +44,15 @@ public final class fransetcore extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
 
-   /* @EventHandler
+    @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.sendMessage(ChatColor.BOLD + "Пожалуйста, ознакомтесь перед игрой с нашими правилами, используя команду: \n" + ChatColor.UNDERLINE + "/rules");
-        player.sendMessage(ChatColor.GRAY + "В случае, если вы уже ознакомились с правилами, чтобы отключить это уведомление используйте команду \n" + ChatColor.UNDERLINE + "/rules off");
-
-    } */
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-
-        Player player = event.getPlayer();
-        ItemStack item = player.getInventory().getItemInMainHand();
-
-        if (item.getType() == Material.AIR) {
-            System.out.println("you break block by hand");
-            event.setCancelled(true);
+        if (!rep.equals(player.getUniqueId())) {
+            rep.put(player.getUniqueId(), 500);
         }
 
+        Integer value = rep.get(player.getUniqueId());
+        player.sendMessage("ваша порядочность: " + value);
     }
 
 
